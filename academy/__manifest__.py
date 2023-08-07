80af77fd80834c7c8c1bc9305fe014a0aec8160e
MagnaWaveActual <softwaredevelopment@magnawavepemf.com>
2023-08-07 20:19:56 +0000

    setting up initial module files

---
+++ academy/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Odoo Academy',
+    'version': '0.0.0.0.1',
+    'summary': 'Import Products from CSV/Excel File, Import Products, Import Product and update Product, Odoo All Import Data',
+    'description': """
+        training for odoo custom modules
+    """,
+    'author': 'MagnaWave',
+    'website': 'www.magnawavepemf.com',
+    'category': 'Custome Modules',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'license': 'OPL-1',
+}
